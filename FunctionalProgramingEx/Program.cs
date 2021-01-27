9e3bc582c89a29af88433eab4fb8e36bcaf6a713
Teddy Aleksov <teddy57603@gmail.com>
2021-01-27 15:53:25 +0200

    Add project files.

---
+++ FunctionalProgramingEx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FunctionalProgramingEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> output = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+            output(input);
+        }
+    }
+}

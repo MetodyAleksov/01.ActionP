bc9e3dab3187f5822e730ebe85632d1876470c11
Teddy Aleksov <teddy57603@gmail.com>
2021-01-27 15:57:00 +0200

    added 02.KnightOfHonor

---
+++ 02.KnightsOfHonor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> output = x => {
+                foreach (var item in x)
+                {
+                    Console.WriteLine($"Sir {item}");
+                }
+            };
+
+            output(input);
+        }
+    }
+}

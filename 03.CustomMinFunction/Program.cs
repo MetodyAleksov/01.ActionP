72d6512742a0ec8ad1bb6643b35c786dbadd6dd8
Teddy Aleksov <teddy57603@gmail.com>
2021-01-27 16:03:03 +0200

    added 03.CustomMinFunction

---
+++ 03.CustomMinFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Min(input));
+        }
+        static int Min(int[] input)
+        {
+            int min = int.MaxValue;
+
+            foreach (var item in input)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+
+            return min;
+        }
+    }
+}

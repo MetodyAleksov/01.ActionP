674a3c9803a4a9fcc38a7ee5fca3060a844ea163
Teddy Aleksov <teddy57603@gmail.com>
2021-01-27 17:17:06 +0200

    added ex 07.PredicateForNames

---
+++ 07.PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, arr));
+        }
+    }
+}

3b49af2a4b640b2017bec56d5bf5afed1ae1bc17
Teddy Aleksov <teddy57603@gmail.com>
2021-01-27 16:19:27 +0200

    added ex 04.FindEvenOrOdd

---
+++ 04.FindEvenOrOdd/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.FindEvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string predicate = Console.ReadLine();
+
+            for (int i = input[0]; i <= input[1]; i++)
+            {
+                PrintEvenOrOdd(predicate, i);
+            }
+        }
+        static void PrintEvenOrOdd(string predicate, int n)
+        {
+            Func<int, bool> func = x => x % 2 == 0;
+
+            if (predicate == "odd")
+            {
+                func = x => x % 2 != 0;
+            }
+
+            if (func(n) == true)
+            {
+                Console.Write($"{n} ");
+            }
+        }
+    }
+}

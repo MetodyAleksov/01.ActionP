9cf1a6af5d148dae8d1f2694fa88c6e36596cf9d
Teddy Aleksov <teddy57603@gmail.com>
2021-01-28 17:39:39 +0200

    added ex 09.ListOfPredicates

---
+++ 08.Custom Comparator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            arr = SortEvenBeforeOdd(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+        static int[] SortEvenBeforeOdd(int[] arr)
+        {
+            int[] finalArr = new int[arr.Length];
+
+            List<int> evenNums = new List<int>();
+            List<int> oddNums = new List<int>();
+
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    evenNums.Add(item);
+                }
+                else
+                {
+                    oddNums.Add(item);
+                }
+            }
+
+            if (evenNums.Count > 0)
+            {
+                for (int i = 0; i < evenNums.Count; i++)
+                {
+                    finalArr[i] = evenNums[i];
+                }
+            }
+            if (oddNums.Count > 0)
+            {
+                for (int i = 0; i < oddNums.Count; i++)
+                {
+                    finalArr[i + evenNums.Count] = oddNums[i];
+                }
+            }
+
+            return finalArr;
+        }
+    }
+}
---
+++ 09.ListOfPredicates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Func<int, int, bool> divisible = (int x, int y) => { 
+                if (x % y == 0)
+                {
+                    return true;
+                }
+                return false;
+                };
+
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isDivisible = true;
+                foreach (int num in arr)
+                {
+                    if (!divisible(i, num))
+                    {
+                        isDivisible = false;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

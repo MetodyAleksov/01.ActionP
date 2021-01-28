3d6956c1336067be273b7af06ba2b3f9ca17ae2a
Teddy Aleksov <teddy57603@gmail.com>
2021-01-28 19:13:31 +0200

    added ex 12.TriFunction

---
+++ 11.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11.PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 12.TriFunction/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isEqualTo = (string name, int num) =>
+            {
+                int sum = 0;
+                foreach (char item in name)
+                {
+                    sum += (int)item;
+                }
+
+                if (sum >= num)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            Action<string[], int> printer = (string[] names, int num) =>
+            {
+                foreach (string name in names)
+                {
+                    if (isEqualTo(name, num))
+                    {
+                        Console.WriteLine(name);
+                        break;
+                    }
+                }
+            };
+
+            printer(names, n);
+        }
+    }
+}
